70b5b332d193d3773bc7d64a28c8b3292963767b
name <email>
2020-11-27 22:16:25 +0900

    modified preprocessing

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,14 +1,28 @@
-import as pd
+import pandas as pd
+import tensorflow as tf
+import numpy as np
 
-df = rd.read_horse_csv(DATA_PATH)
-def create_horse_data(n_race, df):
-    df.sort_values("date", inplace=True)
-    race_df = df.iloc[-1:, :].reset_index(drop=True)
-    columns = race_df.columns
+import read_data as rd
+
+def add_n_race_data(df_race, df_horse, n_race=1):
+    horse_ids = list(set(df_horse['horse_id']))
+    for horse_id in horse_ids:
+        df_horse_tmp = df_horse.query('horse_id == @horse_id')
+        race_ids = list(set(df_horse_tmp['race_id']))
+        for race_id in race_ids:
+            df_race_tmp = df_horse_tmp.query('race_id == @race_id')
+
+
+
+    df_race_horse = df_race_horse.reset_index(drop=True)
+    race_id = df_race_horse.iloc[0, 0]
+    df_horse.sort_values("race_id", inplace=True)
+    df_hourse = df_horse.query('race_id < @race_id')
+    columns = df_horse.columns
     for n in range(1, n_race+1):
         add_columns = list(map(lambda c: c+f"_{n}", columns))
-        add_race = df.iloc[-1-n:-n, :].reset_index(drop=True)
+        add_race = df_horse.iloc[-1-n:-n, :].reset_index(drop=True)
         add_race.columns = add_columns
-        race_df = pd.concat([race_df, add_race], axis=1)
-    return race_df
+        df_race_horse = pd.concat([df_race_horse, add_race], axis=1)
+    return df_race_horse
 
