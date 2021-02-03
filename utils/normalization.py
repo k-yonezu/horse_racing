0daf5ff695b03b3b9338eec7502be548ef8d9dc9
Teruto Endo <endo2014.17@gmail.com>
2021-02-03 20:42:26 +0900

    train.ipynb内の特徴量加工用のコードをutils.normalization.pyに移動、脚質計算用の関数に欠損値への対応を追加

--- utils/normalization.py
+++ utils/normalization.py
@@ -1,7 +1,12 @@
+import datetime
+import re
 import numpy as np
-import pandas as pd
 
 def kyakusitu_code_c(horse_number,half_way_rank):
+    # 欠損値の場合
+    if type(half_way_rank) != str:
+        return half_way_rank
+     
     tuuka= [int(s) for s in half_way_rank.split('-')]
     if len(tuuka)==1:
         if tuuka[0]==1:
@@ -23,3 +28,21 @@ def kyakusitu_code_c(horse_number,half_way_rank):
         else:
             kyakusitu_code=3
     return kyakusitu_code
+
+def extract_place(where_racecourse):
+    place = re.match(r"\d+回(\D+)\d+日目", where_racecourse).groups()[0]
+    return place
+
+def to_seconds(time):
+    if type(time) != str:
+        return time
+    minutes, second_milliseconds = time.split(":")
+    seconds, milliseconds = second_milliseconds.split(".")
+    td = datetime.timedelta(minutes=int(minutes), seconds=int(seconds), milliseconds=int(milliseconds))
+    return td.total_seconds()
+
+def extract_weight(horse_weight):
+    if horse_weight == "計不":
+        return -1
+    weight = re.match(r"(\d+)\([+-]?\d+\)", horse_weight).groups()[0]
+    return weight 
