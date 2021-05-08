9d12d77e2b873e36ac3477da8fb17544d3083790
k-yonezu <31178744+k-yonezu@users.noreply.github.com>
2021-05-08 16:01:07 +0900

    リファクタリング

--- utils/read_data.py
+++ utils/read_data.py
@@ -12,8 +12,8 @@ def read_horse_csv(path):
     df = df.astype({'race_id': str})
     df = df.query('not race_id.str.contains("\(", na=False)', engine='python')
     df = df.astype({'race_id': float, 'horse_id': int, 'popular': 'float16', 'burden_weight': 'float16', 'frame_number': 'int8', 'horse_number': 'int8', 'total_horse_number': 'int8', 'rider_id': 'int32', 'tamer_id': 'int32', 'last_time': 'float16'})
-    df = df.reset_index(drop=True)
-    return df
+
+    return df.reset_index(drop=True)
 
 
 def read_race_csv(path):
@@ -25,8 +25,8 @@ def read_race_csv(path):
     df = df.astype({'race_id': str})
     df = df.query('not race_id.str.contains("\(", na=False)', engine='python')
     df = df.astype({'race_id': float, 'total_horse_number': 'int8', 'frame_number_first': 'int8', 'horse_number_first': 'int8', 'frame_number_second': 'int8', 'horse_number_second': 'int8', 'frame_number_third': 'int8', 'horse_number_third': 'int8'})
-    df = df.reset_index(drop=True)
-    return df
+    
+    return df.reset_index(drop=True)
 
 
 def read_dir_horse_csv(path):
@@ -36,7 +36,7 @@ def read_dir_horse_csv(path):
             print(f)
             df_horse = df_horse.append(read_horse_csv(path + '/' + f + '/'))
             
-    return df_horse
+    return df_horse.reset_index(drop=True)
 
 
 def read_dir_race_csv(path):
@@ -46,7 +46,7 @@ def read_dir_race_csv(path):
             print(f)
             df_race = df_race.append(read_race_csv(path + '/' + f + '/'))
             
-    return df_race
+    return df_race.reset_index(drop=True)
     
 
 
@@ -58,8 +58,8 @@ def read_horse_race_csv(path):
             print(file)
             df = df.append(pd.read_csv(path + '/' + file, index_col=0))
     df = df.astype({'race_id': float})
-    df = df.reset_index(drop=True)
-    return df
+
+    return df.reset_index(drop=True)
 
 
 def read_target_horse_csv(path):
@@ -69,8 +69,8 @@ def read_target_horse_csv(path):
         if ext == '.csv' and 'horse' in base:
             df = pd.concat([df, pd.read_csv(path + '/' + file)])
     df = df.astype({'race_id': float, 'horse_id': int})
-    df = df.reset_index(drop=True)
-    return df
+    
+    return df.reset_index(drop=True)
 
 def read_target_race_csv(path):
     df = pd.DataFrame()
@@ -79,5 +79,5 @@ def read_target_race_csv(path):
         if ext == '.csv' and 'race' in base:
             df = pd.concat([df, pd.read_csv(path + '/'  + file)])
     df = df.astype({'race_id': float})
-    df = df.reset_index(drop=True)
-    return df
+  
+    return df.reset_index(drop=True)
