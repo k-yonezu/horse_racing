aa5f4954a5ec94aa4ac42c6e442ec9cef3a2fa0c
name <email>
2021-05-08 14:07:41 +0900

    data bugfix

--- utils/join_race_data.py
+++ utils/join_race_data.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-def join_row(df_row_target, df, n):
-    for i in range(1, n+1):
-        df_row_i = df.iloc[n-1:n, :].reset_index(drop=True)
+def join_row(df, df_row_target, n_race):
+    df = df.iloc[0:n_race, :]
+    for i in range(1, n_race+1):
+        df_row_i = df.iloc[i-1:i, :].reset_index(drop=True)
         df_row_i.columns = list(map(lambda c: c+f"-{i}", df.columns))
         df_row_target = pd.concat([df_row_target, df_row_i], axis=1)
     return df_row_target
@@ -15,8 +16,8 @@ def join_n_race_for_train_data(df, arr_race_id, n_race=5):
     for race_id in arr_race_id:
         df_tmp = df.query('race_id <= @race_id').sort_values("race_id", ascending=False).reset_index(drop=True)
         df_row_target = df_tmp.iloc[0:1]
-        df_tmp.drop(0, inplace=True)
-        res = res.append(join_row(df_row_target, df_tmp, n_race))
+        df_tmp = df_tmp.drop(0).reset_index(drop=True)
+        res = res.append(join_row(df_tmp, df_row_target, n_race))
 
     return res.reset_index(drop=True)
 
@@ -27,7 +28,7 @@ def join_n_race_for_test_data(df, df_target, n_race=3):
     for horse_id in arr_horse_id:
         df_row_target = df_target.query('horse_id == @horse_id').reset_index(drop=True)
         df_tmp = df.query('horse_id == @horse_id').sort_values("race_id", ascending=False).reset_index(drop=True)
-        res = res.append(join_row(df_row_target, df_tmp, n_race))
+        res = res.append(join_row(df_tmp, df_row_target, n_race))
 
     return res.reset_index(drop=True)
 
