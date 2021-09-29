61b03d87ce4462d3f507cdb3b105b6770cbb4818
Teruto Endo <endo2014.17@gmail.com>
2021-09-29 20:31:18 +0900

    3着以内の馬と1着の馬の正解率を計算する関数を実装

--- utils/correct_rate.py
+++ utils/correct_rate.py
@@ -1,9 +1,51 @@
-def correct_rate(target_ranks: list, predicted_ranks: list) -> float:
-    correct_num = 0
-    for t_r, p_r in zip(target_ranks, predicted_ranks):
-        if target_ranks == predicted_ranks:
-            correct_num += 1
-            
-    rate = correct_num / len(target_ranks)
+import numpy as np
+
+def correct_rate_of_horse_within_3(target_ranks: np.array, predicted_ranks: np.array) -> float:
+    """
+    3着以内に入った馬の正解率を計算
     
-    return rate
+    Parameters
+    ----------
+    target_ranks : numpy.array
+        馬の順位の正解データ
+        
+    predicted_ranks : numpy.array
+        モデルで予測した馬の順位
+    
+    Returns
+    -------
+    correct_rate : float
+        正解率
+    """
+    
+    index_within_3 = target_ranks <= 3
+    corrects = predicted_ranks[index_within_3] <= 3
+
+    correct_rate = corrects.sum() / len(predicted_ranks[index_within_3])
+    
+    return correct_rate
+
+def correct_rate_of_top_horse(target_ranks: np.array, predicted_ranks: np.array) -> float:
+    """
+    一着の馬の正解率を計算
+    
+    Parameters
+    ----------
+    target_ranks : numpy.array
+        馬の順位の正解データ
+        
+    predicted_ranks : numpy.array
+        モデルで予測した馬の順位
+    
+    Returns
+    -------
+    correct_rate : float
+        正解率
+    """
+    
+    index_of_top_horse = target_ranks == 1
+    corrects = predicted_ranks[index_of_top_horse] == 1
+    
+    correct_rate = corrects.sum() / len(predicted_ranks[index_of_top_horse])
+    
+    return correct_rate
