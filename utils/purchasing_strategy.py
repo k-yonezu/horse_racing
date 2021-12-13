6936277f34d8b1a7e347b7ae49dd7f2b02921b06
Teruto Endo <endo2014.17@gmail.com>
2021-12-14 04:38:33 +0900

    全ての馬券の種類についてメソッドを定義

--- utils/purchasing_strategy.py
+++ utils/purchasing_strategy.py
@@ -1,22 +1,68 @@
 from abc import *
 
 class PurchasingStrategy:
+    ticket_types = [
+        "tansho", 
+        "fukusho", 
+        "wakuren", 
+        "umaren", 
+        "umatan", 
+        "wide", 
+        "sanrempuku", 
+        "sanrentan"]
+        
     def __init__(self, predicted_ranks):
         self.predicted_ranks = predicted_ranks
 
     def output_tickets(self, ticket_type):
         tickets = []
-        if ticket_type == "tansyo":
-            tickets = self.tansyo()
-        elif ticket_type == "hukusyo":
-            tickets = self.hukusyo()
+        if ticket_type == self.ticket_types[0]:
+            tickets = self.tansho()
+        elif ticket_type == self.ticket_types[1]:
+            tickets = self.fukusho()
+        elif ticket_type == self.ticket_types[2]:
+            tickets = self.wakuren()
+        elif ticket_type == self.ticket_types[3]:
+            tickets = self.umaren()
+        elif ticket_type == self.ticket_types[4]:
+            tickets = self.umatan()
+        elif ticket_type == self.ticket_types[5]:
+            tickets = self.wide()
+        elif ticket_type == self.ticket_types[6]:
+            tickets = self.sanrempuku()
+        elif ticket_type == self.ticket_types[7]:
+            tickets = self.sanrentan()
         
         return tickets
     
     @abstractmethod
-    def tansyo(self):
+    def tansho(self):
         raise NotImplementedError()
 
     @abstractmethod
-    def hukusyo(self):
+    def fukusho(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def wakuren(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def umaren(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def umatan(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def wide(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def sanrempuku(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def sanrentan(self):
         raise NotImplementedError()
--- utils/top_n_box.py
+++ utils/top_n_box.py
@@ -9,21 +9,40 @@ class TopNBox(PurchasingStrategy):
         super().__init__(predicted_ranks)
         self.n = n
 
-    def tansyo(self):
+    def tansho(self):
         tickets = []
         for i in range(1, self.n+1):
             ticket = np.where(self.predicted_ranks == i)
-            # ticket = (array([something...]), ) 
+            # ticket => (array([something...]), ) 
             # ticket[0] => array[something...]
             tickets.append(ticket[0])
         return tickets
 
-    def hukusyo(self):
-        tickets = self.tansyo()
+    def fukusho(self):
+        tickets = self.tansho()
         return tickets
+
+    def wakuren(self):
+        return []
+
+    def umaren(self):
+        return []
+
+    def umatan(self):
+        return []
+
+    def wide(self):
+        return []
+
+    def sanrempuku(self):
+        return []
+
+    def sanrentan(self):
+        return []
 
 if __name__ == "__main__":
     predicted_ranks = np.array([1, 3, 4, 2, 5, 6])
     top_n_box = TopNBox(predicted_ranks, 2)
-    print(top_n_box.tansyo())
-    print(top_n_box.hukusyo())
+    for t in TopNBox.ticket_types:
+        print(t)
+        print(top_n_box.output_tickets(t))
